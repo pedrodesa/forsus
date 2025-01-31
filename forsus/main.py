e8f32ba81e0c73e09447d2cb6cc70a36824aa271
Pedro de Sá <pedrodesa42@gmail.com>
2025-01-31 09:22:35 -0300

    First version of the project.

---
+++ forsus/main.py
@@ -0,0 +1,21 @@
+from colect import SinanDownloader, load_config
+
+
+# Download dos dados
+def baixar_dados_sinan():
+    # Carregar configurações do arquivo JSON
+    config = load_config('src/config/settings.json')
+
+    agravo = config['agravo']
+    local = config['local']
+    tipos = config['tipos']
+    anos = config['anos']
+
+    # Criando uma instância de SINANDownloader
+    downloader = SinanDownloader(agravo)
+
+    # Chamando o método get_data para baixar os dados
+    downloader.get_data(tipos, local, anos)
+
+
+baixar_dados_sinan()
